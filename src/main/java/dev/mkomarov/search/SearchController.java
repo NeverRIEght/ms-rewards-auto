6078ce6414b990c1975f9c8266f57e732f3f4fa6
Mihail Komarov <michaelkomarov03@gmail.com>
2024-07-15 18:04:15 +0300

    SearchController: remove access modifiers

--- src/main/java/dev/mkomarov/search/SearchController.java
+++ src/main/java/dev/mkomarov/search/SearchController.java
@@ -5,11 +5,11 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public interface SearchController {
-    public static final String WORDS_TXT_PATH = Paths.get("").toAbsolutePath()
+    String WORDS_TXT_PATH = Paths.get("").toAbsolutePath()
             + File.separator + "src"
             + File.separator + "main"
             + File.separator + "resources"
             + File.separator + "words_alpha.txt";
-    public String getRandomWord();
-    public List<String> getWordsList();
+    String getRandomWord();
+    List<String> getWordsList();
 }
